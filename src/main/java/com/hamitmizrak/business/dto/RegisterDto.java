47186ae595259793ce8f15aa33ebfaa5a99dc060
hamitmizrak <hamitmizrak@gmail.com>
2023-01-22 17:29:50 +0300

    ValidationMessages.properties

--- src/main/java/com/hamitmizrak/business/dto/RegisterDto.java
+++ src/main/java/com/hamitmizrak/business/dto/RegisterDto.java
@@ -3,7 +3,9 @@ package com.hamitmizrak.business.dto;
 import lombok.*;
 import lombok.extern.log4j.Log4j2;
 
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 import java.util.Date;
 
@@ -14,15 +16,24 @@ import java.util.Date;
 @Builder
 @Log4j2
 //@SneakyThrows
-
 public class RegisterDto {
 
     private Long id;
-    @NotNull(message = "{}")
-    @Size
+    @NotNull(message = "{register.username.validation.constraints.NotNull.message}")
     private String username;
+
+    @NotNull(message = "{register.surname.validation.constraints.NotNull.message}")
     private String surname;
+
+    @NotNull(message = "{register.email.validation.constraints.NotNull.message}")
+    @Email(message = "{register.email.validation.constraints.property.message}")
+    @Size(max = 150,message = "{register.email.validation.constraints.max.message}")
+    //@RegisterUniqueEmail(message="{register.email.validation.constraints.unique.message}")
     private String email;
+
+    @NotNull(message = "{register.password.validation.constraints.NotNull.message}")
+    @Pattern(regexp = "",message = "{register.password.validation.constraints.pattern.regex.message}")
     private String password;
+
     private Date createdDate;
 }

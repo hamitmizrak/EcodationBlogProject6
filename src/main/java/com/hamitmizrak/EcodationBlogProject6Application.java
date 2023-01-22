6b656faf78779a1dd8cdc3cf9ae662b06838c8fc
hamitmizrak <hamitmizrak@gmail.com>
2023-01-22 18:16:46 +0300

    @SpringBootApplication

--- src/main/java/com/hamitmizrak/EcodationBlogProject6Application.java
+++ src/main/java/com/hamitmizrak/EcodationBlogProject6Application.java
@@ -2,8 +2,32 @@ package com.hamitmizrak;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
+
+
+// Mongo aktif etmek icin
+// @EnableMongoRepositories
+
+// Aspect aktif etmek icin
+// @EnableAspectJAutoProxy(proxyTargetClass = true)
+//
+// Asenkron açmak icin
+// @EnableAsync
+
+// Spring Boot Cache mekanizmasını aktif ediyorum
+// @EnableCaching
+
+//Auditing
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareMethod")
+
+//Entity Class Eğer repository bulamazsa
+// Jpa repoistory bulamazsa @EnableJpaRepositories yazmamız gerekecek
+@EntityScan(basePackages = "com.hamitmizrak.data.entity")
 
 @SpringBootApplication(exclude = {
         SecurityAutoConfiguration.class,
@@ -12,11 +36,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 }
 )
 //@SpringBootApplication
-
-//Auditing
-@EnableJpaAuditing(auditorAwareRef = "auditorAwareMethod")
-
 public class EcodationBlogProject6Application {
+
+    @PostConstruct
+    public void init(){
+        TimeZone.setDefault(TimeZone.getTimeZone("IST"));
+    }
+
     public static void main(String[] args) {
         //Disables headless JOptionPane
         System.setProperty("java.awt.headless", "false");

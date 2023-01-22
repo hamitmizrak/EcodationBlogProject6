b5676ea505209c65921e1a9face11b245c7d8276
hamitmizrak <hamitmizrak@gmail.com>
2023-01-22 16:08:45 +0300

    PasswordEncoderBean

---
+++ src/main/java/com/hamitmizrak/bean/PasswordEncoderBean.java
@@ -0,0 +1,14 @@
+package com.hamitmizrak.bean;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class PasswordEncoderBean {
+
+    @Bean
+    public BCryptPasswordEncoder  passwordEncoderMethod(){
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/com/hamitmizrak/bean/SecurityBean.java
+++
@@ -1,6 +0,0 @@
-package com.hamitmizrak.bean;
-
-public class SecurityBean {
-
-
-}
